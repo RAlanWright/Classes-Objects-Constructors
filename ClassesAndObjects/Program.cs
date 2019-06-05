2bb1e02c7fabad973e7f30cf36dc6b0d3ea98da7
RAlanWright <31636206+RAlanWright@users.noreply.github.com>
2019-06-05 07:59:36 -0500

    Added two books calling class Book with constructor

--- ClassesAndObjects/Program.cs
+++ ClassesAndObjects/Program.cs
@@ -10,6 +10,30 @@ namespace ClassesAndObjects
     {
         static void Main(string[] args)
         {
+
+            /* [ Simplified version ]
+             * Book book1 = new Book
+             *  {
+             *     title = "Goblet of Fire",
+             *      author = "J.K. Rowling",
+             *      pages = 734
+             *  };
+             * 
+             */
+
+            Book book1 = new Book("Goblet of Fire", "J.K. Rowling", 734);
+            //book1.title = "Goblet of Fire";
+            //book1.author = "J.K. Rowling";
+            //book1.pages = 734;
+
+            Book book2 = new Book("Two Towers", "J.R.R. Tolkien", 352);
+            //book2.title = "Two Towers";
+            //book2.author = "J.R.R. Tolkien";
+            //book2.pages = 352;
+
+            Console.WriteLine(book2.author);
+
+            Console.ReadLine();
         }
     }
 }
